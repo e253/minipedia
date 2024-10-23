c0d096b906d1998dfe786a304c1d42392cd1a797
ethan <ethansteere1@gmail.com>
2024-10-23 15:24:33 -0400

    add nice liblzma wrapper, but removed the static build due to missing symbol problems

--- build.zig
+++ build.zig
@@ -5,23 +5,6 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
     const rxml = b.dependency("rapidxml", .{});
-    const xz_tools = b.dependency("xz_tools", .{});
-
-    const lzma = b.addStaticLibrary(.{
-        .name = "lzma",
-        .link_libc = true,
-        .target = target,
-        .optimize = .ReleaseFast,
-    });
-    lzma.addCSourceFiles(.{
-        .root = xz_tools.path(""),
-        .files = &xz_tools_sources,
-        .flags = &xz_tools_c_flags,
-    });
-    inline for (xz_tools_includes) |xz_include| {
-        lzma.addIncludePath(xz_tools.path(xz_include));
-    }
-    b.installArtifact(lzma);
 
     const exe = b.addExecutable(.{
         .name = "main",
@@ -34,7 +17,6 @@ pub fn build(b: *std.Build) void {
     exe.addIncludePath(b.path("src"));
     exe.linkLibC();
     exe.linkLibCpp();
-
     b.installArtifact(exe);
 
     const run_cmd = b.addRunArtifact(exe);
@@ -61,169 +43,197 @@ pub fn build(b: *std.Build) void {
     });
     const run_slice_array_tests = b.addRunArtifact(slice_array_tests);
 
+    const lzma_binding_tests = b.addTest(.{
+        .root_source_file = b.path("src/lzma.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    lzma_binding_tests.linkLibC();
+    lzma_binding_tests.linkSystemLibrary("lzma");
+    const run_lzma_binding_tests = b.addRunArtifact(lzma_binding_tests);
+
     const test_step = b.step("test", "Run Unit Tests");
     test_step.dependOn(&run_wikiparserxml_tests.step);
     test_step.dependOn(&run_slice_array_tests.step);
+    test_step.dependOn(&run_lzma_binding_tests.step);
 }
 
-const xz_tools_includes = [_][]const u8{
-    ".",
-    "lib",
-    "tests",
-    "src/xz",
-    "src/common",
-    "src/liblzma/lz",
-    "src/liblzma/lzma",
-    "src/liblzma/simple",
-    "src/liblzma/common",
-    "src/liblzma/check",
-    "src/liblzma/rangecoder",
-    "src/liblzma/api",
-    "src/liblzma/delta",
-};
-
-const xz_tools_c_flags = [_][]const u8{
-    "-DASSUME_RAM=128",
-    "-DHAVE_CHECK_CRC32=1",
-    "-DHAVE_CHECK_CRC64=1",
-    "-DHAVE_CLOCK_GETTIME=1",
-    "-DHAVE_CLOCK_MONOTONIC=1",
-    "-DHAVE_CPUID_H=1",
-    "-DHAVE_DCGETTEXT=1",
-    "-DHAVE_DECODERS=1",
-    "-DHAVE_DECODER_LZMA1=1",
-    "-DHAVE_DECODER_LZMA2=1",
-    "-DHAVE_DLFCN_H=1",
-    "-DHAVE_ENCODERS=1",
-    "-DHAVE_ENCODER_LZMA1=1",
-    "-DHAVE_ENCODER_LZMA2=1",
-    "-DHAVE_FUNC_ATTRIBUTE_CONSTRUCTOR=1",
-    "-DHAVE_FUTIMENS=1",
-    "-DHAVE_GETOPT_H=1",
-    "-DHAVE_GETOPT_LONG=1",
-    "-DHAVE_GETTEXT=1",
-    "-DHAVE_IMMINTRIN_H=1",
-    "-DHAVE_INTTYPES_H=1",
-    "-DHAVE_LINUX_LANDLOCK=1",
-    "-DHAVE_MBRTOWC=1",
-    "-DHAVE_MF_BT2=1",
-    "-DHAVE_MF_BT3=1",
-    "-DHAVE_MF_BT4=1",
-    "-DHAVE_MF_HC3=1",
-    "-DHAVE_MF_HC4=1",
-    "-DHAVE_POSIX_FADVISE=1",
-    "-DHAVE_STDBOOL_H=1",
-    "-DHAVE_STDINT_H=1",
-    "-DHAVE_STDIO_H=1",
-    "-DHAVE_STDLIB_H=1",
-    "-DHAVE_STRINGS_H=1",
-    "-DHAVE_STRING_H=1",
-    "-DHAVE_STRUCT_STAT_ST_ATIM_TV_NSEC=1",
-    "-DHAVE_SYS_CDEFS_H=1",
-    "-DHAVE_SYS_PARAM_H=1",
-    "-DHAVE_SYS_STAT_H=1",
-    "-DHAVE_SYS_TYPES_H=1",
-    "-DHAVE_UINTPTR_T=1",
-    "-DHAVE_UNISTD_H=1",
-    "-DHAVE_USABLE_CLMUL=1",
-    "-DHAVE_VISIBILITY=1",
-    "-DHAVE__BOOL=1",
-    "-DHAVE__MM_MOVEMASK_EPI8=1",
-    "-DNDEBUG=1",
-    \\-DPACKAGE="\"xz\""
-    ,
-    \\-DPACKAGE_BUGREPORT="\"xz@tukaani.org\""
-    ,
-    \\-DPACKAGE_NAME="\"XZ Utils\""
-    ,
-    \\-DPACKAGE_STRING="\"XZ Utils 5.7.0.alpha\""
-    ,
-    \\-DPACKAGE_TARNAME="\"xz\""
-    ,
-    \\-DPACKAGE_URL="\"https://tukaani.org/xz/\""
-    ,
-    \\-DPACKAGE_VERSION="\"5.7.0.alpha\""
-    ,
-    "-DSIZEOF_SIZE_T=8",
-    "-DSTDC_HEADERS=1",
-    "-DTUKLIB_FAST_UNALIGNED_ACCESS=1",
-    "-DTUKLIB_PHYSMEM_SYSCONF=1",
-    \\-DVERSION="\"5.7.0.alpha\""
-    ,
-};
-
-const xz_tools_sources = [_][]const u8{
-    "src/liblzma/lz/lz_encoder_mf.c",
-    "src/liblzma/lz/lz_encoder.c",
-    "src/liblzma/lz/lz_decoder.c",
-    "src/liblzma/simple/simple_encoder.c",
-    "src/liblzma/simple/simple_coder.c",
-    "src/liblzma/simple/simple_decoder.c",
-    "src/liblzma/rangecoder/price_table.c",
-    "src/liblzma/check/crc64_fast.c",
-    "src/liblzma/check/crc32_fast.c",
-    "src/liblzma/check/check.c",
-    "src/liblzma/delta/delta_encoder.c",
-    "src/liblzma/delta/delta_decoder.c",
-    "src/liblzma/delta/delta_common.c",
-    "src/liblzma/lzma/lzma_encoder_presets.c",
-    "src/liblzma/lzma/lzma_decoder.c",
-    "src/liblzma/lzma/lzma_encoder_optimum_normal.c",
-    "src/liblzma/lzma/lzma2_encoder.c",
-    "src/liblzma/lzma/fastpos_table.c",
-    "src/liblzma/lzma/lzma_encoder.c",
-    "src/liblzma/lzma/lzma_encoder_optimum_fast.c",
-    "src/liblzma/lzma/lzma2_decoder.c",
-    "src/liblzma/common/block_header_encoder.c",
-    "src/liblzma/common/filter_buffer_decoder.c",
-    "src/liblzma/common/stream_decoder.c",
-    "src/liblzma/common/index_hash.c",
-    "src/liblzma/common/block_buffer_encoder.c",
-    "src/liblzma/common/filter_decoder.c",
-    "src/liblzma/common/stream_encoder.c",
-    "src/liblzma/common/stream_flags_common.c",
-    "src/liblzma/common/stream_flags_encoder.c",
-    "src/liblzma/common/auto_decoder.c",
-    "src/liblzma/common/filter_common.c",
-    "src/liblzma/common/outqueue.c",
-    "src/liblzma/common/block_util.c",
-    "src/liblzma/common/alone_encoder.c",
-    "src/liblzma/common/easy_buffer_encoder.c",
-    "src/liblzma/common/block_buffer_decoder.c",
-    "src/liblzma/common/stream_flags_decoder.c",
-    "src/liblzma/common/common.c",
-    "src/liblzma/common/index_decoder.c",
-    "src/liblzma/common/easy_encoder.c",
-    "src/liblzma/common/filter_flags_encoder.c",
-    "src/liblzma/common/string_conversion.c",
-    "src/liblzma/common/index.c",
-    "src/liblzma/common/file_info.c",
-    "src/liblzma/common/filter_encoder.c",
-    "src/liblzma/common/stream_buffer_decoder.c",
-    "src/liblzma/common/vli_size.c",
-    "src/liblzma/common/stream_buffer_encoder.c",
-    "src/liblzma/common/easy_preset.c",
-    "src/liblzma/common/vli_encoder.c",
-    "src/liblzma/common/microlzma_encoder.c",
-    "src/liblzma/common/index_encoder.c",
-    "src/liblzma/common/block_encoder.c",
-    "src/liblzma/common/filter_buffer_encoder.c",
-    "src/liblzma/common/filter_flags_decoder.c",
-    "src/liblzma/common/block_header_decoder.c",
-    "src/liblzma/common/easy_encoder_memusage.c",
-    "src/liblzma/common/lzip_decoder.c",
-    "src/liblzma/common/vli_decoder.c",
-    "src/liblzma/common/alone_decoder.c",
-    "src/liblzma/common/block_decoder.c",
-    "src/liblzma/common/hardware_cputhreads.c",
-    "src/liblzma/common/microlzma_decoder.c",
-    "src/liblzma/common/hardware_physmem.c",
-    "src/liblzma/common/easy_decoder_memusage.c",
-    "src/common/tuklib_mbstr_fw.c",
-    "src/common/tuklib_exit.c",
-    "src/common/tuklib_cpucores.c",
-    "src/common/tuklib_open_stdxxx.c",
-    "src/common/tuklib_progname.c",
-    "src/common/tuklib_physmem.c",
-    "src/common/tuklib_mbstr_width.c",
-};
+// liblzma static build on ice due to missing symbol problems
+//
+//const xz_tools = b.dependency("xz_tools", .{});
+//const lzma = b.addStaticLibrary(.{
+//    .name = "lzma",
+//    .link_libc = true,
+//    .target = target,
+//    .optimize = .ReleaseFast,
+//});
+//lzma.addCSourceFiles(.{
+//    .root = xz_tools.path(""),
+//    .files = &xz_tools_sources,
+//    .flags = &xz_tools_c_flags,
+//});
+//for (xz_tools_includes) |xz_include| {
+//    lzma.addIncludePath(xz_tools.path(xz_include));
+//}
+//lzma.installHeadersDirectory(xz_tools.path("src/liblzma/api"), "", .{});
+//b.installArtifact(lzma);
+// const xz_tools_includes = [_][]const u8{
+//     ".",
+//     "lib",
+//     "tests",
+//     "src/common",
+//     "src/liblzma/lz",
+//     "src/liblzma/lzma",
+//     "src/liblzma/simple",
+//     "src/liblzma/common",
+//     "src/liblzma/check",
+//     "src/liblzma/rangecoder",
+//     "src/liblzma/api",
+//     "src/liblzma/delta",
+// };
+//
+// const xz_tools_c_flags = [_][]const u8{
+//     "-DASSUME_RAM=128",
+//     "-DHAVE_CHECK_CRC32=1",
+//     "-DHAVE_CHECK_CRC64=1",
+//     "-DHAVE_CLOCK_GETTIME=1",
+//     "-DHAVE_CLOCK_MONOTONIC=1",
+//     "-DHAVE_CPUID_H=1",
+//     "-DHAVE_DCGETTEXT=1",
+//     "-DHAVE_DECODERS=1",
+//     "-DHAVE_DECODER_LZMA1=1",
+//     "-DHAVE_DECODER_LZMA2=1",
+//     "-DHAVE_DLFCN_H=1",
+//     "-DHAVE_ENCODERS=1",
+//     "-DHAVE_ENCODER_LZMA1=1",
+//     "-DHAVE_ENCODER_LZMA2=1",
+//     "-DHAVE_FUNC_ATTRIBUTE_CONSTRUCTOR=1",
+//     "-DHAVE_FUTIMENS=1",
+//     "-DHAVE_GETOPT_H=1",
+//     "-DHAVE_GETOPT_LONG=1",
+//     "-DHAVE_GETTEXT=1",
+//     "-DHAVE_IMMINTRIN_H=1",
+//     "-DHAVE_INTTYPES_H=1",
+//     "-DHAVE_LINUX_LANDLOCK=1",
+//     "-DHAVE_MBRTOWC=1",
+//     "-DHAVE_MF_BT2=1",
+//     "-DHAVE_MF_BT3=1",
+//     "-DHAVE_MF_BT4=1",
+//     "-DHAVE_MF_HC3=1",
+//     "-DHAVE_MF_HC4=1",
+//     "-DHAVE_POSIX_FADVISE=1",
+//     "-DHAVE_STDBOOL_H=1",
+//     "-DHAVE_STDINT_H=1",
+//     "-DHAVE_STDIO_H=1",
+//     "-DHAVE_STDLIB_H=1",
+//     "-DHAVE_STRINGS_H=1",
+//     "-DHAVE_STRING_H=1",
+//     "-DHAVE_STRUCT_STAT_ST_ATIM_TV_NSEC=1",
+//     "-DHAVE_SYS_CDEFS_H=1",
+//     "-DHAVE_SYS_PARAM_H=1",
+//     "-DHAVE_SYS_STAT_H=1",
+//     "-DHAVE_SYS_TYPES_H=1",
+//     "-DHAVE_UINTPTR_T=1",
+//     "-DHAVE_UNISTD_H=1",
+//     "-DHAVE_USABLE_CLMUL=1",
+//     "-DHAVE_VISIBILITY=1",
+//     "-DHAVE__BOOL=1",
+//     "-DHAVE__MM_MOVEMASK_EPI8=1",
+//     "-DNDEBUG=1",
+//     \\-DPACKAGE="\"xz\""
+//     ,
+//     \\-DPACKAGE_BUGREPORT="\"xz@tukaani.org\""
+//     ,
+//     \\-DPACKAGE_NAME="\"XZ Utils\""
+//     ,
+//     \\-DPACKAGE_STRING="\"XZ Utils 5.7.0.alpha\""
+//     ,
+//     \\-DPACKAGE_TARNAME="\"xz\""
+//     ,
+//     \\-DPACKAGE_URL="\"https://tukaani.org/xz/\""
+//     ,
+//     \\-DPACKAGE_VERSION="\"5.7.0.alpha\""
+//     ,
+//     "-DSIZEOF_SIZE_T=8",
+//     "-DSTDC_HEADERS=1",
+//     "-DTUKLIB_FAST_UNALIGNED_ACCESS=1",
+//     "-DTUKLIB_PHYSMEM_SYSCONF=1",
+//     \\-DVERSION="\"5.7.0.alpha\""
+//     ,
+// };
+//
+// const xz_tools_sources = [_][]const u8{
+//     "src/liblzma/lz/lz_encoder_mf.c",
+//     "src/liblzma/lz/lz_encoder.c",
+//     "src/liblzma/lz/lz_decoder.c",
+//     "src/liblzma/simple/simple_encoder.c",
+//     "src/liblzma/simple/simple_coder.c",
+//     "src/liblzma/simple/simple_decoder.c",
+//     "src/liblzma/rangecoder/price_table.c",
+//     "src/liblzma/check/crc64_fast.c",
+//     "src/liblzma/check/crc32_fast.c",
+//     "src/liblzma/check/check.c",
+//     "src/liblzma/delta/delta_encoder.c",
+//     "src/liblzma/delta/delta_decoder.c",
+//     "src/liblzma/delta/delta_common.c",
+//     "src/liblzma/lzma/lzma_encoder_presets.c",
+//     "src/liblzma/lzma/lzma_decoder.c",
+//     "src/liblzma/lzma/lzma_encoder_optimum_normal.c",
+//     "src/liblzma/lzma/lzma2_encoder.c",
+//     "src/liblzma/lzma/fastpos_table.c",
+//     "src/liblzma/lzma/lzma_encoder.c",
+//     "src/liblzma/lzma/lzma_encoder_optimum_fast.c",
+//     "src/liblzma/lzma/lzma2_decoder.c",
+//     "src/liblzma/common/block_header_encoder.c",
+//     "src/liblzma/common/filter_buffer_decoder.c",
+//     "src/liblzma/common/stream_decoder.c",
+//     "src/liblzma/common/index_hash.c",
+//     "src/liblzma/common/block_buffer_encoder.c",
+//     "src/liblzma/common/filter_decoder.c",
+//     "src/liblzma/common/stream_encoder.c",
+//     "src/liblzma/common/stream_flags_common.c",
+//     "src/liblzma/common/stream_flags_encoder.c",
+//     "src/liblzma/common/auto_decoder.c",
+//     "src/liblzma/common/filter_common.c",
+//     "src/liblzma/common/outqueue.c",
+//     "src/liblzma/common/block_util.c",
+//     "src/liblzma/common/alone_encoder.c",
+//     "src/liblzma/common/easy_buffer_encoder.c",
+//     "src/liblzma/common/block_buffer_decoder.c",
+//     "src/liblzma/common/stream_flags_decoder.c",
+//     "src/liblzma/common/common.c",
+//     "src/liblzma/common/index_decoder.c",
+//     "src/liblzma/common/easy_encoder.c",
+//     "src/liblzma/common/filter_flags_encoder.c",
+//     "src/liblzma/common/string_conversion.c",
+//     "src/liblzma/common/index.c",
+//     "src/liblzma/common/file_info.c",
+//     "src/liblzma/common/filter_encoder.c",
+//     "src/liblzma/common/stream_buffer_decoder.c",
+//     "src/liblzma/common/vli_size.c",
+//     "src/liblzma/common/stream_buffer_encoder.c",
+//     "src/liblzma/common/easy_preset.c",
+//     "src/liblzma/common/vli_encoder.c",
+//     "src/liblzma/common/microlzma_encoder.c",
+//     "src/liblzma/common/index_encoder.c",
+//     "src/liblzma/common/block_encoder.c",
+//     "src/liblzma/common/filter_buffer_encoder.c",
+//     "src/liblzma/common/filter_flags_decoder.c",
+//     "src/liblzma/common/block_header_decoder.c",
+//     "src/liblzma/common/easy_encoder_memusage.c",
+//     "src/liblzma/common/lzip_decoder.c",
+//     "src/liblzma/common/vli_decoder.c",
+//     "src/liblzma/common/alone_decoder.c",
+//     "src/liblzma/common/block_decoder.c",
+//     "src/liblzma/common/hardware_cputhreads.c",
+//     "src/liblzma/common/microlzma_decoder.c",
+//     "src/liblzma/common/hardware_physmem.c",
+//     "src/liblzma/common/easy_decoder_memusage.c",
+//     "src/common/tuklib_mbstr_fw.c",
+//     "src/common/tuklib_exit.c",
+//     "src/common/tuklib_cpucores.c",
+//     "src/common/tuklib_open_stdxxx.c",
+//     "src/common/tuklib_progname.c",
+//     "src/common/tuklib_physmem.c",
+//     "src/common/tuklib_mbstr_width.c",
+// };
---
+++ src/lzma.zig
@@ -0,0 +1,250 @@
+const c = @cImport(@cInclude("lzma.h"));
+const std = @import("std");
+
+// Allocator
+const Allocator = c.lzma_allocator;
+/// DOES NOT FREE, must use external arena to do this instead
+fn lzma_allocator_free(_: ?*anyopaque, _: ?*anyopaque) callconv(.C) void {}
+fn lzma_allocator_alloc(_zig_a: ?*anyopaque, _: usize, size: usize) callconv(.C) ?*anyopaque {
+    const zig_a: *std.mem.Allocator = @alignCast(@ptrCast(_zig_a));
+    const alloc_slice = zig_a.alloc(u8, size) catch |err| switch (err) {
+        error.OutOfMemory => @panic("LZMA Allocator ran out of memory!"),
+    };
+    return alloc_slice.ptr;
+}
+/// ALLOCATOR MUST BE AN ARENA!!
+pub fn from_zig_allocator(a: *const std.mem.Allocator) Allocator {
+    return .{
+        .alloc = lzma_allocator_alloc,
+        .free = lzma_allocator_free,
+        .@"opaque" = @constCast(a),
+    };
+}
+
+// Streams
+pub const Stream = c.lzma_stream;
+/// 0 initializes a `Stream` struct
+pub fn stream_init() Stream {
+    return .{
+        .next_in = null,
+        .avail_in = 0,
+        .total_in = 0,
+        .next_out = null,
+        .avail_out = 0,
+        .total_out = 0,
+        .allocator = null,
+        .reserved_ptr1 = null,
+        .reserved_ptr2 = null,
+        .reserved_ptr3 = null,
+        .reserved_ptr4 = null,
+        .seek_pos = 0,
+        .reserved_int2 = 0,
+        .reserved_int3 = 0,
+        .reserved_int4 = 0,
+        .reserved_enum1 = c.LZMA_RESERVED_ENUM,
+        .reserved_enum2 = c.LZMA_RESERVED_ENUM,
+    };
+}
+pub const end = c.lzma_end;
+
+// LZMA2 Configuration
+pub const OptionsLZMA = c.lzma_options_lzma;
+/// Defaults to -9e, the highest lzma cli setting
+pub fn options_lzma_default() OptionsLZMA {
+    return .{
+        .dict_size = 64_000_000,
+        .lc = 3,
+        .lp = 0,
+        .pb = 2,
+        .mode = c.LZMA_MODE_NORMAL,
+        .nice_len = 273,
+        .mf = c.LZMA_MF_BT4,
+        .depth = 512,
+    };
+}
+
+// LZMA Filters
+pub const FILTERS_MAX: usize = @intCast(c.LZMA_FILTERS_MAX);
+pub const Filter = c.lzma_filter;
+pub const FilterId = enum(u64) {
+    lzma2 = c.LZMA_FILTER_LZMA2,
+    filters_end = c.LZMA_VLI_UNKNOWN,
+};
+
+// Checks
+pub const check = enum(u32) {
+    none = c.LZMA_CHECK_NONE,
+    crc32 = c.LZMA_CHECK_CRC32,
+    crc64 = c.LZMA_CHECK_CRC64,
+    sha256 = c.LZMA_CHECK_SHA256,
+};
+
+// Stream Initialization
+pub const StreamInitError = error{
+    ProgError,
+    OptionsError,
+    MemError,
+};
+
+pub fn stream_encoder(s: *Stream, filters: [FILTERS_MAX + 1]Filter, ch: check) StreamInitError!void {
+    const r: c.lzma_ret = c.lzma_stream_encoder(s, &filters, @intFromEnum(ch));
+    switch (r) {
+        c.LZMA_PROG_ERROR => return StreamInitError.ProgError,
+        c.LZMA_OPTIONS_ERROR => return StreamInitError.OptionsError,
+        c.LZMA_MEM_ERROR => return StreamInitError.MemError,
+        c.LZMA_OK => return,
+        else => unreachable,
+    }
+}
+
+pub fn stream_decoder(s: *Stream) StreamInitError!void {
+    const r: c.lzma_ret = c.lzma_stream_decoder(s, c.UINT64_MAX, 0);
+    switch (r) {
+        c.LZMA_PROG_ERROR => return StreamInitError.ProgError,
+        c.LZMA_OPTIONS_ERROR => return StreamInitError.OptionsError,
+        c.LZMA_MEM_ERROR => return StreamInitError.MemError,
+        c.LZMA_OK => return,
+        else => unreachable,
+    }
+}
+
+// Encoding / Decoding Execution
+pub const Action = enum(u32) {
+    run = c.LZMA_RUN,
+    finish = c.LZMA_FINISH,
+};
+
+pub const LzmaError = error{
+    NoCheck,
+    UnsupportedCheck,
+    GetCheck,
+    MemError,
+    MemLimitError,
+    FormatError,
+    OptionsError,
+    DataError,
+    BufError,
+    ProgError,
+    SeekNeeded,
+};
+
+/// StreamEnd and Ok are both valid,
+/// but mean different things
+pub const LzmaOk = enum {
+    StreamEnd,
+    Ok,
+};
+
+/// Wraps `lzma_code` with nice error handling
+pub fn execute(s: *Stream, a: Action) LzmaError!LzmaOk {
+    const r: c.lzma_ret = c.lzma_code(s, @intFromEnum(a));
+    switch (r) {
+        c.LZMA_NO_CHECK => return LzmaError.NoCheck,
+        c.LZMA_UNSUPPORTED_CHECK => return LzmaError.UnsupportedCheck,
+        c.LZMA_GET_CHECK => return LzmaError.GetCheck,
+        c.LZMA_MEM_ERROR => return LzmaError.MemError,
+        c.LZMA_MEMLIMIT_ERROR => return LzmaError.MemLimitError,
+        c.LZMA_FORMAT_ERROR => return LzmaError.FormatError,
+        c.LZMA_OPTIONS_ERROR => return LzmaError.OptionsError,
+        c.LZMA_DATA_ERROR => return LzmaError.DataError,
+        c.LZMA_BUF_ERROR => return LzmaError.BufError,
+        c.LZMA_PROG_ERROR => return LzmaError.ProgError,
+        c.LZMA_SEEK_NEEDED => return LzmaError.SeekNeeded,
+        c.LZMA_STREAM_END => return LzmaOk.StreamEnd,
+        c.LZMA_OK => return LzmaOk.Ok,
+        else => unreachable,
+    }
+}
+
+/// Single call lzma2 -9e compress from input data `in`
+/// to output buffer `out`
+///
+/// `a` MUST BE AN ARENA since it will be passed to liblzma
+///
+/// Otherwise, `a` can be null and the libc allocator is used instead
+///
+/// Return value points to `out` but with an adjusted size
+pub fn compress(a_opt: ?*const std.mem.Allocator, in: []const u8, out: []u8) ![]u8 {
+    var compression_opts = options_lzma_default();
+    const filters = [FILTERS_MAX + 1]Filter{
+        .{ .id = @intFromEnum(FilterId.lzma2), .options = &compression_opts },
+        .{ .id = @intFromEnum(FilterId.filters_end) },
+        .{},
+        .{},
+        .{},
+    };
+
+    var s = stream_init();
+
+    if (a_opt) |a| {
+        s.allocator = &from_zig_allocator(a);
+    }
+
+    try stream_encoder(&s, filters, .crc32);
+
+    s.next_in = @constCast(in.ptr);
+    s.avail_in = in.len;
+    s.next_out = out.ptr;
+    s.avail_out = out.len;
+
+    std.debug.assert(try execute(&s, .finish) == .StreamEnd);
+
+    end(&s);
+
+    return out[0 .. out.len - s.avail_out];
+}
+
+pub fn decompress(a_opt: ?*const std.mem.Allocator, in: []const u8, out: []u8) ![]u8 {
+    var s = stream_init();
+
+    if (a_opt) |a| {
+        s.allocator = &from_zig_allocator(a);
+    }
+
+    try stream_decoder(&s);
+
+    s.next_in = @constCast(in.ptr);
+    s.avail_in = in.len;
+    s.next_out = out.ptr;
+    s.avail_out = out.len;
+
+    std.debug.assert(try execute(&s, .finish) == .StreamEnd);
+
+    end(&s);
+
+    return out[0 .. out.len - s.avail_out];
+}
+
+test "Stream Initialization" {
+    const s = stream_init();
+    try std.testing.expect(s.next_in == null);
+}
+
+test "Hello, World! Compress" {
+    var arena = std.heap.ArenaAllocator.init(std.testing.allocator);
+    defer arena.deinit();
+    const alloc = arena.allocator();
+
+    const in: []const u8 = "Hello, World!";
+    var out = try alloc.alloc(u8, 256);
+    const expected = [_]u8{ 0xfd, 0x37, 0x7a, 0x58, 0x5a, 0x0, 0x0, 0x1, 0x69, 0x22, 0xde, 0x36, 0x02, 0x00, 0x21, 0x01, 0x1c, 0x00, 0x00, 0x00, 0x10, 0xcf, 0x58, 0xcc, 0x01, 0x00, 0x0c, 0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x2c, 0x20, 0x57, 0x6f, 0x72, 0x6c, 0x64, 0x21, 0x0, 0x0, 0x0, 0x0, 0xd0, 0xc3, 0x4a, 0xec, 0x0, 0x1, 0x21, 0xd, 0x75, 0xdc, 0xa8, 0xd2, 0x90, 0x42, 0x99, 0x0d, 0x1, 0x0, 0x0, 0x0, 0x0, 0x1, 0x59, 0x5a };
+
+    out = try compress(&alloc, in, out);
+
+    try std.testing.expect(out.len == 68);
+    try std.testing.expectEqualSlices(u8, &expected, out);
+}
+
+test "Hello, World! Decompress" {
+    var arena = std.heap.ArenaAllocator.init(std.testing.allocator);
+    defer arena.deinit();
+    const alloc = arena.allocator();
+
+    const in = [_]u8{ 0xfd, 0x37, 0x7a, 0x58, 0x5a, 0x0, 0x0, 0x1, 0x69, 0x22, 0xde, 0x36, 0x02, 0x00, 0x21, 0x01, 0x1c, 0x00, 0x00, 0x00, 0x10, 0xcf, 0x58, 0xcc, 0x01, 0x00, 0x0c, 0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x2c, 0x20, 0x57, 0x6f, 0x72, 0x6c, 0x64, 0x21, 0x0, 0x0, 0x0, 0x0, 0xd0, 0xc3, 0x4a, 0xec, 0x0, 0x1, 0x21, 0xd, 0x75, 0xdc, 0xa8, 0xd2, 0x90, 0x42, 0x99, 0x0d, 0x1, 0x0, 0x0, 0x0, 0x0, 0x1, 0x59, 0x5a };
+    var out = try alloc.alloc(u8, 256);
+    const expected: []const u8 = "Hello, World!";
+
+    out = try decompress(&alloc, &in, out);
+
+    try std.testing.expectEqualStrings(expected, out);
+}
