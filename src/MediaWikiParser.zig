ac8e440197029fa14e7d1410327f86ce621051ce
ethan <ethansteere1@gmail.com>
2024-10-26 20:45:02 -0400

    html tag parsing with attributes and some tests. remains a WIP ... needs support for parsing child tags and more robust testing.

--- src/MediaWikiParser.zig
+++ src/MediaWikiParser.zig
@@ -17,7 +17,7 @@ pub const MWNode = union(MWNodeType) {
     wiki_link: []const u8,
     external_link: ExternalLinkCtx,
     heading: HeadingCtx,
-    html_tag: []const u8,
+    html_tag: HtmlTagCtx,
     html_entity: []const u8,
     argument: []const u8,
     text: []const u8,
@@ -34,11 +34,22 @@ pub const HeadingCtx = struct {
     level: usize,
 };
 
+pub const HtmlTagCtx = struct {
+    pub const HtmlTagAttr = struct {
+        name: []const u8,
+        value: []const u8,
+    };
+
+    tag_name: []const u8,
+    text: ?[]const u8,
+    attrs: ?[]HtmlTagAttr,
+    children: ?[]HtmlTagCtx,
+};
+
 pub const MWParser = struct {
     /// Contains list of nodes which will eventually contain parsed information
     nodes: std.ArrayList(MWNode),
     raw_wikitext: []const u8,
-    /// Slices of text we want, these are referenced within each node
     /// This must be arena, ideally wrapping a fixed buffer allocator
     a: std.mem.Allocator,
 
@@ -56,9 +67,15 @@ pub const MWParser = struct {
         IncompleteArgument,
         IncompleteHeading,
         IncompleteHtmlEntity,
+        UnclosedHtmlComment,
+        InvalidHtmlTag,
     };
 
     /// iterates through characters and dispatches to node specific parsing functions
+    ///
+    /// HTML entities `&lt;` and `&gt;` must be replaced with `<` and `>` to be interpreted as html tags.
+    ///
+    /// anything starting with `&` is assumed to be an HTML entity. Use `&amp;` instead.
     pub fn parse(self: *Self) !void {
         var i: usize = 0;
         var cur_text_node = self.raw_wikitext;
@@ -99,27 +116,31 @@ pub const MWParser = struct {
                     }
                 },
 
-                // html entity that could be &lt;
-                '&' => {
-                    if (nextEql(self.raw_wikitext, "&lt;", i)) {
-                        if (nextEql(self.raw_wikitext, "!--", i + "&lt;".len)) {
-                            // skip html comment
-                            i += 1;
-                        } else {
-                            // parse html tag
-                            i += 1;
-                        }
-                    } else {
-                        // places entity in text becuase I suspect the failure could false trigger on regular text
-                        if (cur_text_node.len > 0) {
-                            try self.nodes.append(.{ .text = cur_text_node });
-                        }
-
-                        i = try self.parseHtmlEntity(i);
-
-                        cur_text_node = self.raw_wikitext[i..];
-                        cur_text_node.len = 0;
+                '<' => {
+                    if (cur_text_node.len > 0) {
+                        try self.nodes.append(.{ .text = cur_text_node });
                     }
+
+                    if (nextEql(self.raw_wikitext, "!--", i + 1)) {
+                        i = try self.skipHtmlComment(i + "<!--".len);
+                    } else {
+                        i = try self.parseHtmlTag(i);
+                    }
+
+                    cur_text_node = self.raw_wikitext[i..];
+                    cur_text_node.len = 0;
+                },
+
+                // html entity
+                '&' => {
+                    if (cur_text_node.len > 0) {
+                        try self.nodes.append(.{ .text = cur_text_node });
+                    }
+
+                    i = try self.parseHtmlEntity(i);
+
+                    cur_text_node = self.raw_wikitext[i..];
+                    cur_text_node.len = 0;
                 },
 
                 // headings
@@ -149,6 +170,171 @@ pub const MWParser = struct {
         if (cur_text_node.len > 0) {
             try self.nodes.append(.{ .text = cur_text_node });
         }
+    }
+
+    /// **Internal WIP**
+    ///
+    /// `i` should point to the opening `<`
+    ///
+    /// TODO: handle nesting. Errors right now!
+    fn parseHtmlTag(self: *Self, start: usize) !usize {
+        var i = try advance(start, "<".len, self.raw_wikitext);
+        if (isWhiteSpace(self.raw_wikitext[i]))
+            return ParseError.InvalidHtmlTag;
+
+        const tag_name_start = i;
+        i = skip(self.raw_wikitext, node_name_pred, i) orelse return ParseError.InvalidHtmlTag;
+        if (i == tag_name_start)
+            return ParseError.InvalidHtmlTag;
+        const tag_name = self.raw_wikitext[tag_name_start..i];
+
+        i = skip(self.raw_wikitext, whitespace_pred, i) orelse return ParseError.InvalidHtmlTag;
+
+        // attempt to find attributes
+        var attrs = std.ArrayList(HtmlTagCtx.HtmlTagAttr).init(self.a);
+        while (attribute_name_pred(self.raw_wikitext[i])) {
+            if (i >= self.raw_wikitext.len)
+                return ParseError.InvalidHtmlTag;
+
+            // extract attr name
+            const attr_name_start = i;
+            i = skip(self.raw_wikitext, attribute_name_pred, i) orelse return ParseError.InvalidHtmlTag;
+            if (attr_name_start == i)
+                return ParseError.InvalidHtmlTag;
+            const attr_name = self.raw_wikitext[attr_name_start..i];
+
+            // skip whitespace after attr name
+            i = skip(self.raw_wikitext, whitespace_pred, i) orelse return ParseError.InvalidHtmlTag;
+
+            // skip '='
+            if (self.raw_wikitext[i] != '=')
+                return ParseError.InvalidHtmlTag;
+            i = try advance(i, "=".len, self.raw_wikitext);
+
+            // skip whitespace after =
+            i = skip(self.raw_wikitext, whitespace_pred, i) orelse return ParseError.InvalidHtmlTag;
+
+            // skip quote and remember if it was ' or "
+            const quote = self.raw_wikitext[i];
+            if (i + 1 == self.raw_wikitext.len or (quote != '\'' and quote != '"'))
+                return ParseError.InvalidHtmlTag;
+            i = try advance(i, 1, self.raw_wikitext);
+
+            // get attr value
+            const attr_value_start = i;
+            switch (quote) {
+                '\'' => i = skip(self.raw_wikitext, attribute_value_single_quote_pred, i) orelse return ParseError.InvalidHtmlTag,
+                '"' => i = skip(self.raw_wikitext, attribute_value_double_quote_pred, i) orelse return ParseError.InvalidHtmlTag,
+                else => unreachable,
+            }
+            const attr_value = self.raw_wikitext[attr_value_start..i];
+
+            // skip last quote
+            i = try advance(i, 1, self.raw_wikitext);
+
+            try attrs.append(.{ .name = attr_name, .value = attr_value });
+
+            // skip whitespace after attr name
+            i = skip(self.raw_wikitext, whitespace_pred, i) orelse return ParseError.InvalidHtmlTag;
+        }
+        if (i + 1 >= self.raw_wikitext.len)
+            return ParseError.InvalidHtmlTag;
+
+        // Handle self closing tag
+        if (self.raw_wikitext[i] == '/') {
+            if (self.raw_wikitext[i + 1] != '>') {
+                return ParseError.InvalidHtmlTag;
+            }
+            if (attrs.items.len > 0) {
+                try self.nodes.append(.{ .html_tag = .{
+                    .tag_name = tag_name,
+                    .text = null,
+                    .children = null,
+                    .attrs = try attrs.toOwnedSlice(),
+                } });
+            } else {
+                try self.nodes.append(.{ .html_tag = .{
+                    .tag_name = tag_name,
+                    .text = null,
+                    .children = null,
+                    .attrs = null,
+                } });
+            }
+            return try advance(i, "/>".len, self.raw_wikitext);
+        }
+
+        // skip closing '>'
+        if (self.raw_wikitext[i] != '>')
+            return ParseError.InvalidHtmlTag;
+        i = try advance(i, 1, self.raw_wikitext);
+
+        const content_start = i;
+
+        while (i < self.raw_wikitext.len) : (i += 1) {
+            const ch = self.raw_wikitext[i];
+            switch (ch) {
+                '<' => {
+                    // end content
+                    const content_end = i;
+
+                    // skip '>'
+                    i = try advance(i, 1, self.raw_wikitext);
+                    if (self.raw_wikitext[i] != '/')
+                        return ParseError.InvalidHtmlTag;
+                    // skip '/'
+                    i = try advance(i, 1, self.raw_wikitext);
+
+                    // get close tag name
+                    const close_tag_name_start = i;
+                    i = skip(self.raw_wikitext, node_name_pred, i) orelse return ParseError.InvalidHtmlTag;
+                    const close_tag_name = self.raw_wikitext[close_tag_name_start..i];
+
+                    // validate
+                    if (!std.mem.eql(u8, close_tag_name, tag_name))
+                        return ParseError.InvalidHtmlTag;
+
+                    i = skip(self.raw_wikitext, whitespace_pred, i) orelse return ParseError.InvalidHtmlTag;
+                    if (self.raw_wikitext[i] != '>')
+                        return ParseError.InvalidHtmlTag;
+
+                    if (attrs.items.len == 0) {
+                        try self.nodes.append(.{ .html_tag = .{
+                            .tag_name = tag_name,
+                            .text = self.raw_wikitext[content_start..content_end],
+                            .attrs = null,
+                            .children = null,
+                        } });
+                    } else {
+                        try self.nodes.append(.{ .html_tag = .{
+                            .tag_name = tag_name,
+                            .text = self.raw_wikitext[content_start..content_end],
+                            .attrs = try attrs.toOwnedSlice(),
+                            .children = null,
+                        } });
+                    }
+
+                    return i + 1;
+                },
+                else => {},
+            }
+        }
+
+        return ParseError.InvalidHtmlTag;
+    }
+
+    /// moves to i to after the comment,
+    /// or returns `ParseError.UnclosedHtmlComment` if none is found
+    fn skipHtmlComment(self: *Self, start: usize) !usize {
+        var i = start;
+        while (i < self.raw_wikitext.len) : (i += 1) {
+            const ch = self.raw_wikitext[i];
+            if (ch == '-') {
+                if (nextEql(self.raw_wikitext, "-->", i)) {
+                    return i + "-->".len;
+                }
+            }
+        }
+        return ParseError.UnclosedHtmlComment;
     }
 
     /// attempts to find html entity from '&' start character
@@ -271,11 +457,90 @@ pub const MWParser = struct {
     }
 };
 
+/// Advances `i` until `continue_pred` returns false
+///
+/// If end of `buf` is reached, returns `null`
+///
+/// Predicate calls are inlined for performance
+fn skip(buf: []const u8, continue_pred: fn (u8) bool, _i: usize) ?usize {
+    var i = _i;
+    while (i < buf.len) : (i += 1) {
+        if (!@call(.always_inline, continue_pred, .{buf[i]})) {
+            return i;
+        }
+    }
+    return null;
+}
+
+/// Stops on ' ', '\n', '\r', '\t', '/', '>', '?', '\0'
+fn node_name_pred(ch: u8) bool {
+    switch (ch) {
+        ' ', '\n', '\r', '\t', '/', '>', '?', 0 => return false,
+        else => return true,
+    }
+}
+
+/// Continues on ' ', '\t', '\n', '\r'
+fn whitespace_pred(ch: u8) bool {
+    switch (ch) {
+        ' ', '\t', '\n', '\r' => return true,
+        else => return false,
+    }
+}
+
+/// Stops on ' ', '\n', '\r', '\t', '/', '>', '?', '\0', '='
+fn attribute_name_pred(ch: u8) bool {
+    switch (ch) {
+        ' ', '\n', '\r', '\t', '/', '>', '?', 0, '=' => return false,
+        else => return true,
+    }
+}
+
+/// Stops on '\''
+fn attribute_value_single_quote_pred(ch: u8) bool {
+    return ch != '\'';
+}
+
+/// Stops on '"'
+fn attribute_value_double_quote_pred(ch: u8) bool {
+    return ch != '"';
+}
+
+const AdvanceError = error{AdvanceBeyondBuffer};
+/// Safely advance buffer index `i` by count
+///
+/// Returns `error.AdvanceBeyondBuffer` if i+count goes out of bounds
+inline fn advance(i: usize, count: usize, buf: []const u8) !usize {
+    if (i + count < buf.len) {
+        return i + count;
+    } else {
+        return AdvanceError.AdvanceBeyondBuffer;
+    }
+}
+
+inline fn isWhiteSpace(ch: u8) bool {
+    return ch == ' ' or ch == '\n' or ch == '\t' or ch == '\r';
+}
+
+/// advance to the first non whitespace
+///
+/// **`\n` and `\r` are considered whitespace**
+inline fn skipWhitespace(buf: []const u8, i: usize) ?usize {
+    while (i < buf.len) : (i += 1) {
+        switch (buf[i]) {
+            ' ', '\t', '\n', '\r' => continue,
+            else => return i,
+        }
+    }
+
+    return null;
+}
+
 /// returns `true` if the needle is present starting at i.
 ///
 /// `false` if out of bounds
 inline fn nextEql(buf: []const u8, needle: []const u8, i: usize) bool {
-    if (i + needle.len < buf.len) {
+    if (i + needle.len <= buf.len) {
         return std.mem.eql(u8, buf[i..][0..needle.len], needle);
     }
     return false;
@@ -287,6 +552,7 @@ test "Trivial nextEql" {
         \\
     ;
     try std.testing.expect(nextEql(wikitext, "{{{", 0));
+    try std.testing.expect(nextEql(wikitext, "}}\n", 6));
     try std.testing.expect(!nextEql(wikitext, "{{{", 7));
 }
 
@@ -450,5 +716,153 @@ test "Correctly Parses HTML Entity" {
             .html_entity => |he| try std.testing.expectEqualStrings(entities[i], he),
             else => unreachable,
         }
+    }
+}
+
+test "Errors on unclosed html comment" {
+    const wikitext = "<!-- Blah --";
+
+    var arena = std.heap.ArenaAllocator.init(std.testing.allocator);
+    defer arena.deinit();
+    const a = arena.allocator();
+
+    var wp = MWParser.init(a, wikitext);
+    const e = wp.parse();
+
+    try std.testing.expectError(MWParser.ParseError.UnclosedHtmlComment, e);
+}
+
+test "Skips HTML Comment" {
+    const wikitext = "<!-- Blah Blah -->";
+
+    var arena = std.heap.ArenaAllocator.init(std.testing.allocator);
+    defer arena.deinit();
+    const a = arena.allocator();
+
+    var wp = MWParser.init(a, wikitext);
+    try wp.parse();
+}
+
+test "Parses Well Formed Heading With Some Text and a Comment" {
+    const wikitext =
+        \\= Blah Blah Blah =
+        \\Blah <!-- Blah Blah --> Blah Blah
+        \\Some more Blah
+        \\
+    ;
+
+    var arena = std.heap.ArenaAllocator.init(std.testing.allocator);
+    defer arena.deinit();
+    const a = arena.allocator();
+
+    var wp = MWParser.init(a, wikitext);
+    try wp.parse();
+
+    try std.testing.expect(wp.nodes.items.len == 3);
+
+    try std.testing.expectEqualStrings(@tagName(wp.nodes.items[0]), "heading");
+    try std.testing.expectEqualStrings(@tagName(wp.nodes.items[1]), "text");
+    try std.testing.expectEqualStrings(@tagName(wp.nodes.items[2]), "text");
+
+    switch (wp.nodes.items[0]) {
+        .heading => |h| {
+            try std.testing.expectEqualStrings(" Blah Blah Blah ", h.heading);
+            try std.testing.expect(h.level == 1);
+        },
+        else => unreachable,
+    }
+
+    switch (wp.nodes.items[1]) {
+        .text => |text| try std.testing.expectEqualStrings("Blah ", text),
+        else => unreachable,
+    }
+
+    switch (wp.nodes.items[2]) {
+        .text => |text| try std.testing.expectEqualStrings(" Blah Blah\nSome more Blah\n", text),
+        else => unreachable,
+    }
+}
+
+test "HTML Tag Trivial Correct" {
+    const wikitext = "<ref>Hello</ref>";
+
+    var arena = std.heap.ArenaAllocator.init(std.testing.allocator);
+    defer arena.deinit();
+    const a = arena.allocator();
+
+    var wp = MWParser.init(a, wikitext);
+    try wp.parse();
+
+    try std.testing.expect(wp.nodes.items.len == 1);
+
+    switch (wp.nodes.items[0]) {
+        .html_tag => |t| {
+            try std.testing.expectEqualStrings("ref", t.tag_name);
+            try std.testing.expectEqualStrings("Hello", t.text.?);
+        },
+        else => unreachable,
+    }
+}
+
+test "HTML Tag Wierd Spacing" {
+    const case1: []const u8 = "<ref>Hello</ref>";
+    const case2: []const u8 =
+        \\<ref   >Hello</ref>
+    ;
+    const case3: []const u8 =
+        \\<ref   >Hello</ref   >
+    ;
+    const case4: []const u8 =
+        \\<ref   
+        \\  >Hello</ref   
+        \\>
+    ;
+    const case5: []const u8 =
+        \\<ref       
+        \\  >Hello</ref   
+        \\>
+    ;
+
+    const cases = [_][]const u8{ case1, case2, case3, case4, case5 };
+
+    for (cases) |case| {
+        var arena = std.heap.ArenaAllocator.init(std.testing.allocator);
+        defer arena.deinit();
+        const a = arena.allocator();
+
+        var wp = MWParser.init(a, case);
+        try wp.parse();
+
+        try std.testing.expect(wp.nodes.items.len == 1);
+
+        switch (wp.nodes.items[0]) {
+            .html_tag => |t| {
+                try std.testing.expectEqualStrings("ref", t.tag_name);
+                try std.testing.expectEqualStrings("Hello", t.text.?);
+            },
+            else => unreachable,
+        }
+    }
+}
+
+test "Decodes HTML Tag Attributes Correctly" {
+    const wikitext = "<ref kind='web'>citation</ref>";
+
+    var arena = std.heap.ArenaAllocator.init(std.testing.allocator);
+    defer arena.deinit();
+    const a = arena.allocator();
+
+    var wp = MWParser.init(a, wikitext);
+    try wp.parse();
+
+    switch (wp.nodes.items[0]) {
+        .html_tag => |t| {
+            try std.testing.expect(t.attrs.?.len == 1);
+            try std.testing.expectEqualStrings("kind", t.attrs.?[0].name);
+            try std.testing.expectEqualStrings("web", t.attrs.?[0].value);
+            try std.testing.expectEqualStrings("ref", t.tag_name);
+            try std.testing.expectEqualStrings("citation", t.text.?);
+        },
+        else => unreachable,
     }
 }
